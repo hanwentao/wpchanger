89403793a539c9db8e0e5bd79c7692f4957eb6cd
Wentao Han <wentao.han@gmail.com>
2011-04-28 23:59:32 +0800

    deal with exceptions

--- wpchanger.py
+++ wpchanger.py
@@ -34,20 +34,27 @@ def write_file(path, data):
     f.close()
 
 def main(argv):
-    logging.info('reading wallpaper index page')
-    page = read_url(WALLPAPER_INDEX_URL)
-    uri = WALLPAPER_URI_PATTERN.findall(page)[0]
-    url = WALLPAPER_URL_BASE + uri
-    filename = os.path.basename(uri)
-    logging.debug('wallpaper filename "%s"', filename)
-    path = os.path.join(WALLPAPER_CACHE, filename)
-    logging.info('reading wallpaper image data')
-    data = read_url(url)
-    logging.info('writing wallpaper image data')
-    write_file(path, data)
-    logging.info('changing wallpaper')
-    app('Finder').desktop_picture.set(mactypes.File(path))
-    logging.info('all done')
+    try:
+        logging.info('reading wallpaper index page')
+        page = read_url(WALLPAPER_INDEX_URL)
+        uri = WALLPAPER_URI_PATTERN.findall(page)[0]
+        url = WALLPAPER_URL_BASE + uri
+        filename = os.path.basename(uri)
+        logging.debug('wallpaper filename "%s"', filename)
+        path = os.path.join(WALLPAPER_CACHE, filename)
+        logging.info('reading wallpaper image data')
+        data = read_url(url)
+        logging.info('writing wallpaper image data')
+        write_file(path, data)
+        logging.info('changing wallpaper')
+        app('Finder').desktop_picture.set(mactypes.File(path))
+        logging.info('all done')
+    except IOError:
+        logging.error('network problem')
+        return 1
+    except IndexError:
+        logging.error('wallpaper not found')
+        return 1
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
