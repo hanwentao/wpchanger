9bb1bdc657d1797b96c0149c7c2c27944bb746c1
Wentao Han <wentao.han@gmail.com>
2012-01-05 14:46:18 +0800

    port to gnome

--- wpchanger.py
+++ wpchanger.py
@@ -5,6 +5,7 @@
 # All rights reserved
 #
 
+import commands
 import logging
 import os
 import os.path
@@ -12,20 +13,18 @@ import random
 import re
 import sys
 import urllib
-
-from cStringIO import StringIO
-
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 import Image
 
-from appscript import app
-from appscript import mactypes
-
 WALLPAPER_URL_BASE = 'http://interfacelift.com'
-WALLPAPER_INDEX_URL = WALLPAPER_URL_BASE + '/wallpaper/downloads/random/widescreen/1440x900/'
-WALLPAPER_CACHE = '/Users/hanwentao/Downloads/wallpapers'
+WALLPAPER_INDEX_URL = WALLPAPER_URL_BASE + '/wallpaper/downloads/random/hdtv/1080p/'
+WALLPAPER_CACHE = os.path.expanduser('~/Downloads/wallpapers')
 WALLPAPER_URI_PATTERN = re.compile(r'<a href="(/wallpaper/\w+/\w+\.jpg)">')
 
-logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
+logging.basicConfig(format='%(asctime)s %(levelname).1s: %(message)s',
                     level=logging.DEBUG)
 
 def read_url(url):
@@ -35,9 +34,16 @@ def read_url(url):
     return content
 
 def write_file(path, data):
+    dir, filename = os.path.split(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
     f = open(path, 'wb')
     f.write(data)
     f.close()
+
+def set_wallpaper_gnome(path):
+    cmdline = 'gconftool-2 -t string -s /desktop/gnome/background/picture_filename "%s"' % path
+    status, output = commands.getstatusoutput(cmdline)
 
 def main(argv):
     success = False
@@ -62,7 +68,7 @@ def main(argv):
         else:
             logging.debug('wallpaper exists')
         logging.info('changing wallpaper')
-        app('Finder').desktop_picture.set(mactypes.File(path))
+        set_wallpaper_gnome(path)
         success = True
         logging.info('all done')
     except IOError:
@@ -74,7 +80,7 @@ def main(argv):
         filename = random.choice(filenames)
         path = os.path.join(WALLPAPER_CACHE, filename)
         logging.info('changing wallpaper from cache')
-        app('Finder').desktop_picture.set(mactypes.File(path))
+        set_wallpaper_gnome(path)
         logging.info('all done')
 
 if __name__ == '__main__':
